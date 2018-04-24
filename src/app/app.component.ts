65e9e38c6c0671dfeb2da823be02a379ddb11fae
Unnsin <vladlesnoj405@gmail.com>
2018-04-24 18:37:53 +0300

    API commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
+  NowPage:number;
+  constructor(public Service: DataService){}
   title = 'app';
+  movies = [];
+  ngOnInit(){
+    this.NowPage = 1;
+    this.Service.GetMovies(1).subscribe(data => {
+      this.movies = data.results.slice(0,12); 
+    },error=>{},
+    ()=>{} );
+  }
+  GetPage(index:number, flag:boolean){
+    if(index >= 1){
+    this.NowPage = index;
+    this.movies = this.Service.GetPage(index-1);
+    }
+  }
+  GetStart(){
+    this.movies = this.Service.GetPage(0);
+    this.NowPage = 1;
+  }
+  GetEnd(){
+    let len = this.Service.GetLenth();
+    console.log(len);
+    this.NowPage = len;
+    this.movies = this.Service.GetPage(len-1);
+  }
 }
---
+++ src/app/data-service.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+@Injectable()
+export class DataService {
+  Movies = []; 
+  Page_col:number[] = [];
+  key = 'ebea8cfca72fdff8d2624ad7bbf78e4c'; 
+  Page:number = 2;
+  constructor(private http: HttpClient) { 
+
+      this.GetMovies(1).subscribe(data => {
+      for(var i=0;i<data.results.length;i++){
+        this.Movies.push(data.results[i]);
+      }
+      },
+      error=>{},
+      ()=>{
+        this.GetMovies(2).subscribe(data => {
+        for(var i=0;i<data.results.length;i++){
+          this.Movies.push(data.results[i]);
+        }
+        },
+        error=>{},
+        ()=>{console.log(this.Movies);this.GetPageCol();});
+      });
+      
+  }
+
+  GetMovies(Page:number):Observable<any>{
+    return this.http.get('http://api.themoviedb.org/3/movie/now_playing?api_key=' + this.key+"&page="+ Page);
+  }
+  GetPageCol(){
+    this.Page_col.length = 0;
+    var col = this.Movies.length;
+    for(var i=0;i<col/12;i++){
+      this.Page_col.push(i+1);
+    }
+    console.log(this.Page_col);
+    return this.Page_col;
+  }
+
+  GetPage(numb:number){
+    if(numb == this.Page_col.length-2){
+      this.GetMovies(this.Page+1).subscribe(data => {
+        for(var i=0;i<data.results.length;i++){
+          this.Movies.push(data.results[i]);
+        }
+        },
+        error=>{},
+        ()=>{console.log(this.Movies);this.GetPageCol(); this.Page++;});
+    }
+    return this.Movies.slice(numb*12,(numb*12)+12);
+  }
+  GetLenth(){
+    return this.Page_col.length;
+  }
+}
